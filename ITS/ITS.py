5c12ac5b08a58f981fa243ba49c09cb36dce737d
Houssam ROUGA <houssam.rouga@telecom-sudparis.eu>
2025-03-26 02:33:29 +0100

    upgrade 1.2

--- ITS/ITS.py
+++ ITS/ITS.py
@@ -19,19 +19,19 @@ class Entity:
 
     def __init__(self, address: Address):
         self.__Private_Key=ec.generate_private_key(ec.SECP256R1())
-        self.__Public_Key=self.__Private_Key.public_Key()
+        self.__Public_Key=self.__Private_Key.public_key()
         self.address=address
         self.connected_Entities: dict[str,Entity]={}
         self.buffer:list[mini_packet]=[]
 
-    def send(destination: 'Entity',binary_msg: bin):
+    def send(self, destination: "Entity",binary_msg: bin):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((destination.address.ip_address, destination.address.Port))
             s.sendall(binary_msg)
             return 1
     
     # this fnct maybe will not be in use 
-    def listen(address: Address):
+    def listen(self, address: Address):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((address.ip_address, address.Port))
             s.listen(100)
@@ -45,20 +45,23 @@ class Entity:
             s.bind((address.ip_address, address.Port))
             while True:
                 s.listen(100)
+                print("ITS LEASTNING") ##
                 conn, addr = s.accept()
-                source_address:Address=None
+                print("ITS ACCEPTED")  ##
+                source_address = Address(None,None)
                 source_address.ip_address,source_address.Port=addr
                 with conn:
                     data = conn.recv(4096)
-                    packet:mini_packet=None
+                    packet = mini_packet(None,None)
                     packet.address,packet.data=source_address,data
                     self.buffer.append(packet)
+                    print(self.buffer[0].data.decode('utf-8')) #to delete
     
     def get_Public_Key(self):
         return self.__Public_Key
 
-    def get_Exchanged_Key(this,entity:"Entity"):
-        return this.__Private_Key.exchange(ec.ECDH(), entity.get_Public_Key())
+    def get_Exchanged_Key(self,entity:"Entity"):
+        return self.__Private_Key.exchange(ec.ECDH(), entity.get_Public_Key())
 
     #To see later
     def encrypt_message():
@@ -75,15 +78,17 @@ class Entity:
                 return name
         return None
     
-    """
     @abstractmethod
     def packet_forwarding(self):
         pass
-
+    
+    @abstractmethod
+    def forward_and_empty_buffer(self):
+        pass
+    
     @abstractmethod
     def start(self):
         pass
-    """
     # A ne pas décommenter pour le moment
     """
     def send_request(address:Address, binary_msg):
@@ -144,7 +149,8 @@ class  Registration_Authority (Entity):
             pass # implement a fonction that forward the data to VEH
         else:   #the entity is a vehicule implement then a fonction to send LTC in data to LTCA
             pass
-    
+
+
     def forward_and_empty_buffer(self,buffer: list[mini_packet]):
         while True:
             if len(buffer) != 0:
@@ -197,7 +203,7 @@ class Link_Authority (Entity):
         forwarding_thread.start()
 
 
-class Pseudo_Certificate_Authority (Entity):
+class Pseudonym_Certificate_Authority (Entity):
     def __init__(self, address):
         super().__init__(address)
         self.connected_vehicule=0
@@ -278,7 +284,7 @@ class Long_Term_Certificate_Authority (Entity):
 
 
 
-class vehicule (Entity):
+class Vehicule (Entity):
     def __init__(self, address):
         super().__init__(address)
     
@@ -315,6 +321,68 @@ class vehicule (Entity):
     def start(self):
         listening_thread = threading.Thread(target=self.listen_and_fill_buffer, args=(self.address,self.buffer,))
         listening_thread.start()
-        forwarding_thread = threading.Thread(target=self.forward_and_empty_buffer, args=(self.buffer,))
-        forwarding_thread.start()
+        
+        #forwarding_thread = threading.Thread(target=self.forward_and_empty_buffer, args=(self.buffer,))
+        #forwarding_thread.start()
 
+
+if __name__ == '__main__':
+    
+    # Address INIT
+    ra_address   = Address('localhost', 5000)
+    la1_address  = Address('localhost', 5001)
+    la2_address  = Address('localhost', 5002)
+    LTCA_address = Address('localhost', 5003)
+    PCA_address  = Address('localhost', 5004)
+    veh_address  = Address('localhost', 5005)
+
+    # Entities creation
+    RA = Registration_Authority(ra_address)
+    VEH = Vehicule(veh_address)
+    LTCA = Long_Term_Certificate_Authority(LTCA_address)
+    PCA = Pseudonym_Certificate_Authority(PCA_address)
+    LA1 = Link_Authority(la1_address)
+    LA2 = Link_Authority(la2_address)
+
+    # Recognition (linking entities)
+    RA.add_vehicule(VEH)
+    RA.add_LA1(LA1)
+    RA.add_LA2(LA2)
+    RA.add_LTCA(LTCA)
+    RA.add_PCA(PCA)
+
+    LTCA.add_PCA(PCA)
+    LTCA.add_vehicule(VEH)
+
+    VEH.add_RA(RA)
+    VEH.add_PCA(PCA)
+    VEH.add_LA1(LA1)
+    VEH.add_LA2(LA2)
+    VEH.add_PCA(PCA)
+
+    PCA.add_LA1(RA)
+    PCA.add_LA2(LA2)
+    PCA.add_RA(RA)
+    PCA.add_vehicule(VEH)
+
+    LA1.add_PCA(PCA)
+    LA1.add_RA(RA)
+    LA1.add_vehicule(VEH)
+
+    LA2.add_PCA(PCA)
+    LA2.add_RA(RA)
+    LA2.add_vehicule(VEH)
+
+
+    # Start services
+    RA.start()
+    VEH.start()
+    LTCA.start()
+    PCA.start()
+    LA1.start()
+    LA2.start()
+
+    time.sleep(4)
+    VEH.send(RA,'hello'.encode('utf-8'))
+    VEH.send(RA,'hello'.encode('utf-8'))
+    VEH.send(RA,'hello'.encode('utf-8'))
